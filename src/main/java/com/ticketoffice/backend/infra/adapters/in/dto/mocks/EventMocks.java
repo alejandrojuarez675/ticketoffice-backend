b1662f6f4a03f957fcf7818b13f7e420d837729d
alejandro juarez <alejandrojuarez675@gmail.com>
2025-05-14 19:19:47 -0300

    add in memory data repository

--- src/main/java/com/ticketoffice/backend/infra/adapters/in/dto/mocks/EventMocks.java
+++ src/main/java/com/ticketoffice/backend/infra/adapters/in/dto/mocks/EventMocks.java
@@ -1,16 +1,10 @@
 package com.ticketoffice.backend.infra.adapters.in.dto.mocks;
 
-import com.ticketoffice.backend.infra.adapters.in.dto.response.events.EventDetailPageResponse;
 import com.ticketoffice.backend.infra.adapters.in.dto.response.events.EventListResponse;
 import com.ticketoffice.backend.infra.adapters.in.dto.response.events.EventResponse;
 import com.ticketoffice.backend.infra.adapters.in.dto.response.events.EventLightResponse;
 import com.ticketoffice.backend.infra.adapters.in.dto.response.events.SocialMediasResponse;
 import com.ticketoffice.backend.infra.adapters.in.dto.shared.EventListItemDTO;
-import com.ticketoffice.backend.infra.adapters.in.dto.shared.ImageDTO;
-import com.ticketoffice.backend.infra.adapters.in.dto.shared.LocationDTO;
-import com.ticketoffice.backend.infra.adapters.in.dto.shared.OrganizerDTO;
-import com.ticketoffice.backend.infra.adapters.in.dto.shared.PriceDTO;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class EventMocks {
@@ -59,54 +53,6 @@ public class EventMocks {
             "https://youtube.com/event1",
             "https://tiktok.com/event1",
             "https://website.com/event1"
-    );
-
-    public final static String LSDF = "https://facebook.com/event1";
-
-    public static final EventDetailPageResponse EVENT_DETAIL_PAGE_RESPONSE = new EventDetailPageResponse(
-            "cbb46e0f-1014-41f9-bf63-ba1203197ce8",
-            "Concierto de Trueno",
-            LocalDateTime.of(2025, 6, 7, 19, 0),
-            new LocationDTO(
-                    "Movistar Arena",
-                    "Dg. 61c #26-36",
-                    "Bogotá"
-            ),
-            new ImageDTO(
-                "https://movistararena.co/wp-content/uploads/2025/02/trueno-2025-4.jpg",
-                "Concierto de Trueno"
-            ),
-            List.of(
-                    new PriceDTO(
-                            "001b2f30-9a84-45e1-9345-518bea8a77c8",
-                            100000.0,
-                            "$",
-                            "VIP",
-                            false
-                    ),
-                    new PriceDTO(
-                            "cd85b222-2adf-414d-aa26-6a0fb7c87beb",
-                            30000.0,
-                            "$",
-                            "General",
-                            false
-                    )
-            ),
-            "Trueno es uno de los grandes artistas latinoamericanos de nuestros tiempos. Un rapero que comenzó destacándose en las batallas de freestyle en su natal Buenos Aires y hoy ha logrado el reconocimiento internacional además de acumular millones de oyentes en plataformas digitales. Este 7 de junio el Movistar Arena será testigo del prime de uno de los nuevos grandes del universo urbano.",
-            List.of(
-                    "Prohibido el ingreso de menores de edad",
-                    "No se permite el ingreso de alimentos y bebidas",
-                    "No se permite el ingreso de cámaras fotográficas"
-            ),
-            new OrganizerDTO(
-                    "78cd80e2-023f-4b38-a409-d4a20f2d4ac7",
-                    "Movistar Arena",
-                    "https://movistararena.co/",
-                    new ImageDTO(
-                            "https://movistararena.co/wp-content/uploads/2023/08/logo_blanco.png",
-                            "Organizer Logo"
-                    )
-            )
     );
 
     public static EventListItemDTO eventListItemDTO = new EventListItemDTO(
