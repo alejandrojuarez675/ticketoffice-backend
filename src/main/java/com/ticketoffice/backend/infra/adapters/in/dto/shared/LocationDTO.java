1dec86df5ba0fea493aa0d0a282c6fda5dfa5e45
alejandro juarez <alejandrojuarez675@gmail.com>
2025-05-22 11:26:42 -0300

    fix swagger

--- src/main/java/com/ticketoffice/backend/infra/adapters/in/dto/shared/LocationDTO.java
+++ src/main/java/com/ticketoffice/backend/infra/adapters/in/dto/shared/LocationDTO.java
@@ -4,13 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record LocationDTO(
 
-        @Schema(name = "The name of the location", example = "Movistar Arena")
+        @Schema(description = "The name of the location", example = "Movistar Arena")
         String name,
 
-        @Schema(name = "The address of the location", example = "Dg. 61c #26-36")
+        @Schema(description = "The address of the location", example = "Dg. 61c #26-36")
         String address,
 
-        @Schema(name = "The city of the location", example = "Bogotá")
+        @Schema(description = "The city of the location", example = "Bogotá")
         String city
 
 ) {
