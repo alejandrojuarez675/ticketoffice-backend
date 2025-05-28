277603f711abea40f04189afedc9b2eeccc0bb67
alejandrosju_meli <alejandrosebastian.juarez@mercadolibre.com>
2025-05-28 20:44:55 -0300

    update swagger

--- src/main/java/com/ticketoffice/backend/infra/adapters/in/dto/request/UserLoginRequest.java
+++ src/main/java/com/ticketoffice/backend/infra/adapters/in/dto/request/UserLoginRequest.java
@@ -1,7 +1,13 @@
 package com.ticketoffice.backend.infra.adapters.in.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UserLoginRequest (
+
+    @Schema(description = "The username of the user", example = "user123")
     String username,
+
+    @Schema(description = "The password of the user", example = "password123")
     String password
 ) {
 }
