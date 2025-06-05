761a11be104aa42f3c39d8ecc095c0e42d85c043
alejandrosju_meli <alejandrosebastian.juarez@mercadolibre.com>
2025-06-05 09:55:06 -0300

    add endpoint to register a purchase

--- src/main/java/com/ticketoffice/backend/infra/adapters/in/controller/checkout/CheckoutController.java
+++ src/main/java/com/ticketoffice/backend/infra/adapters/in/controller/checkout/CheckoutController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -74,7 +75,7 @@ public class CheckoutController {
     )
     @PostMapping("/session/{sessionId}/buy")
     public ResponseEntity<Void> buyTicket(
-            @RequestParam String sessionId,
+            @PathVariable String sessionId,
             @RequestBody BuyTicketsRequest request
     ) throws BadRequestException {
         new BuyTicketsRequestValidator().validate(request);
