7cbe292e9481a1e8d6c534f7c2b9dd67cd9c8a23
Alejandro Juarez <alejandrojuarez675@gmail.com>
2025-10-20 09:54:04 -0300

    fix encryption of sellers

--- src/main/java/com/ticketoffice/backend/application/services/PasswordEncoder.java
+++ src/main/java/com/ticketoffice/backend/application/services/PasswordEncoder.java
@@ -2,15 +2,34 @@ package com.ticketoffice.backend.application.services;
 
 import java.util.Optional;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
 public class PasswordEncoder {
     public String encode(String password) {
-        return password;
+        if (password == null) {
+            throw new IllegalArgumentException("Password cannot be null");
+        }
+        try {
+            String salt = "lksadfjksadflkfjsadlsfadjl";
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 128);
+            SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] hash = f.generateSecret(spec).getEncoded();
+            Base64.Encoder enc = Base64.getEncoder();
+            return enc.encodeToString(hash);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean matches(String password, String passwordEncoded) {
         return Optional.ofNullable(password)
                 .map(this::encode)
-                .map(x -> passwordEncoded.equals(password))
+                .map(passwordEncoded::equals)
                 .orElse(false);
     }
 }
